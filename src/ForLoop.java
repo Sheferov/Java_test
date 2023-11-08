7d85abb05e087d1d618e9e3e40e2ab03197f3d98
Sheferov <sheferov02@yandex.ru>
2023-11-08 06:47:24 +0300

    deleted

--- src/ForLoop.java
+++
@@ -1,7 +0,0 @@
-public class ForLoop {
-    public static void main(String[] args) {
-        for(int i = 1000;i<5000;i = i+100){
-            System.out.println("пися ");  //i++ то же самое, что и i == i+1 (true/false)
-        }
-    }
-}
